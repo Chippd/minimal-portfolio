c9a24c1d358817706b1ac067ea1ce8b6a9110f73
Chris Dermody <chrisdermody1@gmail.com>
2017-11-18 22:47:21 +0000

    got flex working as it should, need logic to add/remove elements

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,17 +1,42 @@
 
 
+// as the user scrolls, poll the .main-row element for its .scrollTop position
+// reference: https://developer.mozilla.org/en-US/docs/Web/Events/scroll
+// reference for above reference (lol): http://www.html5rocks.com/en/tutorials/speed/animations/
 
 
 
-// var controller = new ScrollMagic.Controller();
+function doSomething(scroll_pos) {
+  // do something with the scroll position
+  console.log('scroll_pos:', scroll_pos);
+}
 
-// var scene = new ScrollMagic.Scene({
-// 		triggerElement: '#pinned-trigger1',
-// 	  offset: 100, // start scene after scrolling for 100px
-// 	  duration: 400 // pin the element for 400px of scrolling
-// 	})
-// .setPin('#pinned-element1');
+let ticking;
 
-// // Add Scene to ScrollMagic Controller
-// controller.addScene(scene);
+window.addEventListener('scroll', function(e) {
 
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+     
+    ticking = true;
+
+  }
+  
+});
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM fully loaded and parsed");
+
+  // when content is fully loaded, get height of screen
+  let screenHeight = screen.height
+
+  console.log('screenHeight:', screenHeight);
+
+});
