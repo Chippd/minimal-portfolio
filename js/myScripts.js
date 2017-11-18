f8a3bedfa2be25f0c0afcb9aa87498ee37c2eba0
Chris Dermody <chrisdermody1@gmail.com>
2017-11-18 22:18:33 +0000

    got barebones layout done, need to add scroll logic to handle transitions

---
+++ js/myScripts.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+// var controller = new ScrollMagic.Controller();
+
+// var scene = new ScrollMagic.Scene({
+// 		triggerElement: '#pinned-trigger1',
+// 	  offset: 100, // start scene after scrolling for 100px
+// 	  duration: 400 // pin the element for 400px of scrolling
+// 	})
+// .setPin('#pinned-element1');
+
+// // Add Scene to ScrollMagic Controller
+// controller.addScene(scene);
+
