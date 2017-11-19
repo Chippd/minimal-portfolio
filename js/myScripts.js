3d90dc4354a9188d14db2111b8ef6b2af2ab8d34
Chris Dermody <chrisdermody1@gmail.com>
2017-11-19 00:27:20 +0000

    added background colour changing too

--- js/myScripts.js
+++ js/myScripts.js
@@ -2,27 +2,26 @@
 
 
 // some global variables
-let pageHeight, ticking, screenHeight, currentEl;
+let pageHeight, ticking, screenHeight, currentEl, projectTextArr, projectImgArr, colorArr;
 
-
+colorArr = [
+	"#d8dcff",
+	"#aeadf0",
+	"#c38d94"
+]
 
 // as the user scrolls, poll the .main-row element for its .scrollTop position
 // reference: https://developer.mozilla.org/en-US/docs/Web/Events/scroll
 // reference for above reference (lol): http://www.html5rocks.com/en/tutorials/speed/animations/
 function doSomething(scroll_pos) {
 
-  // calculate where in page user is relative to projects in elements array
-
   // how many times does pageHeight go into scrollPos
   let x = Math.round(scroll_pos / screenHeight);
-  console.log('screenHeight goes into scroll_pos this many times:', x);
 
   // set appropriate element if not already
   if(currentEl !== x){
   	applyElement(x);
   }
-
-
 }
 
 function checkScroll(){
@@ -41,70 +40,55 @@ function checkScroll(){
 	}
 }
 
+// every time user scrolls, check if we need to change element visibility
+// seperating as we need to call checkScroll on page load too
 window.addEventListener('scroll', function(e) {
 
   checkScroll()
   
 });
 
-// do stuff on page load
+
+
+// When the page is loaded
 document.addEventListener("DOMContentLoaded", function(event) {
-  
-  console.log("DOM fully loaded and parsed");
 
-  checkScroll();
-
-  // when content is fully loaded, get height of screen
+  // Get height of screen
   screenHeight = screen.height
-  console.log('screenHeight:', screenHeight);
 
-  // set total page height depending on number of elements in elements[]
-  pageHeight = screenHeight * elements.length
-  console.log("pageHeight is:", pageHeight);
+  // Get all projects on page and populate arrays
+  projectTextArr = document.getElementsByClassName('project-text');
+  projectImgArr = document.getElementsByClassName('project-img');
+
+  // set total page height depending on number of projects
+  pageHeight = screenHeight * projectTextArr.length;
   document.getElementById('body').style.height = pageHeight+"px";
+
+  // Check the scroll position, show appropriate project
+  checkScroll();
 
 });
 
-let elements = [
-	{
-		"text1": "Chris Dermody",
-		"text2": "Developer, element 0",
-		"img-src": "./img/profiler.png",
-		"bg-color": "#d8dcff"
-	},
-	{
-		"text1": "Jobb.ie",
-		"text2": "A quick description of jobbie..., element 1",
-		"img-src": "./img/jobbie1.png",
-		"bg-color": "#aeadf0"
-	},
-	{
-		"text1": "Element2",
-		"text2": "",
-		"img-src": "./img/profiler.png",
-		"bg-color": "#d8dcff"
-	},
-	{
-		"text1": "Element3",
-		"text2": "A quick description of jobbie...",
-		"img-src": "./img/jobbie1.png",
-		"bg-color": "#aeadf0"
+
+function applyElement(x){
+
+	// for each project and project-img
+	// hide all
+	for (var i = projectImgArr.length - 1; i >= 0; i--) {
+		projectImgArr[i].style.display = "none"
 	}
-]
 
+	for (var i = projectTextArr.length - 1; i >= 0; i--) {
+		projectTextArr[i].style.display = "none"
+	}
 
-function applyElement(i){
+	// Show appropriate project
+	projectTextArr[x].style.display = "initial";
+	projectImgArr[x].style.display = "initial";
 
-	console.log('setting element:', i)
+	// Set color on body
+	document.getElementById('body').style.background = colorArr[x];
 
-	// set the element of the appropraite element index
-	document.getElementById('text1').innerHTML = elements[i].text1;
-
-	document.getElementById('text2').innerHTML = elements[i].text2;
-
-	document.getElementById("project-img").src = elements[i]['img-src'];
-
-	currentEl = i
 
 }
 
